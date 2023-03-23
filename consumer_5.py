2fde2b915b6824b4dc398053d16fbd18b16be56f
Arunbbhat <arun.b.bhat@gmail.com>
2023-03-23 10:18:02 +0530

    8th

--- consumer_5.py
+++ consumer_5.py
@@ -22,44 +22,9 @@ def config_sorter(conf,use):
         props = {key:conf[key] for key in req}
         return props
 
-
-
-# def read_kafka_producer_config(config_file):
-#     conf = {}
-#     with open(config_file) as fh:
-#         for line in fh:
-#             line = line.strip()
-#             if len(line) != 0 and line[0] != "#":
-#                 parameter, value = line.strip().split('=', 1)
-#                 if parameter == 'key.serializer' or parameter == 'value.serializer':
-#                     conf[parameter] = KafkaJsonSchemaSerializer(eval(value))
-#                 elif parameter.endswith('.schema'):
-#                     conf[parameter] = eval(value)
-#                 else:
-#                     conf[parameter] = value.strip()
-#     return conf
-
-def write_to_csv(file_location, data):
-    # Open the file for writing
-
-    
-            
-            
-
-            # Save the DataFrame to a CSV file
+def write_to_csv(file_location, data):          
     data.to_csv(f'{file_location}', index=False)
     print(f"\nData written to {file_location} successfully.")
-    # with open(file_location, 'w', newline='') as csv_file:
-        
-    #     # Create a writer object
-    #     writer = csv.writer(csv_file)
-
-    #     # Write the data to the CSV file
-    #     for row in data:
-    #         writer.writerow([row])
-
-    # print(f"Data written to {file_location} successfully.")
-
 class User(object):
     """
     User record
@@ -93,18 +58,15 @@ import time
 from confluent_kafka import Consumer, TopicPartition, KafkaError,DeserializingConsumer,Producer
 from confluent_kafka.schema_registry.json_schema import JSONDeserializer
 from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.serialization import SerializationContext, MessageField
-from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import SerializationContext, MessageField,StringDeserializer
+from confluent_kafka.schema_registry.avro import AvroSerializer,AvroDeserializer
 import random
 import json
 import csv
 import numpy as np
 import pandas as pd
-import subprocess
-import os
-
-
 import argparse
+import re
 
 if __name__ == '__main__':
 
@@ -128,12 +90,10 @@ if __name__ == '__main__':
     parser.add_argument('--result_dump_local_filepath',help='file path to store consumer output ')
     parser.add_argument('--result_dump_producer_config',help=' configuration.properties file that contains settings and properties used to configure a Kafka producer application to dump consumer results' )
     parser.add_argument('--output_topic',help='Kafka topic to dump consumer output')
-    parser.add_argument('--consumer_schema_json',default='None',help='File path of consumer JsonSchema')
-    parser.add_argument('--confluent_sr_config',default='None',help='Enter url of conflunt schema registary')
-    #parser.add_argument('--confluent_sr_apikey',help='Enter api key of conflunet SR')
-    #parser.add_argument('--confluent_sr_apisecret',help='Enter api secret of conflunent SR ')
-
-
+    #parser.add_argument('--consumer_schema_json',default='None',help='File path of consumer JsonSchema')
+    #parser.add_argument('--confluent_sr_config',default='None',help='Enter url of conflunt schema registary')
+    parser.add_argument('--value_deserializer',required=True,choices={'AvroDeserializer','JSONSchemaDeserializer','StringDeserializer','JSONDeserializer'},help='Deserializer class for value that implements the org.apache.kafka.common.serialization.Deserializer interface.')
+    parser.add_argument('--key-deserializer',choices={'AvroDeserializer','JSONSchemaDeserializer','StringDeserializer','JSONDeserializer'},help='Deserializer class for key that implements the org.apache.kafka.common.serialization.Deserializer interface.')
 
 
 
@@ -151,146 +111,127 @@ if __name__ == '__main__':
     producer_prop=args.result_dump_producer_config
     output_topic=args.output_topic
     schema_location=args.consumer_schema_json
-    confluent_sr_config=args.confluent_sr_config
-    #api_key=args.confluent_sr_apikey
-    #api_secret=args.confluent_sr_apisecret
+    #confluent_sr_config=args.confluent_sr_config
+    value_deserializer = args.value_deserializer
 
-    print("\n\nConsumer has started!!\n")
+    pattern = r"\"ordertime\":(\d+)"
 
-
-    #print("consumer configaration used:",props)
-
-#     schema_str = """
-#     {
-#   "$schema": "http://json-schema.org/draft-07/schema#",
-#   "type": "object",
-#   "properties": {
-#     "ordertime": {
-#       "type": "integer",
-#       "description": "The time the order was placed, represented as a Unix timestamp in milliseconds."
-#     },
-#     "orderid": {
-#       "type": "integer",
-#       "description": "The unique identifier for the order."
-#     },
-#     "itemid": {
-#       "type": "string",
-#       "description": "The unique identifier for the item being ordered."
-#     },
-#     "orderunits": {
-#       "type": "number",
-#       "description": "The number of units of the item being ordered, with up to 10 decimal places."
-#     },
-#     "address": {
-#       "type": "object",
-#       "description": "The shipping address for the order.",
-#       "properties": {
-#         "city": {
-#           "type": "string",
-#           "description": "The name of the city for the shipping address."
-#         },
-#         "state": {
-#           "type": "string",
-#           "description": "The name of the state for the shipping address."
-#         },
-#         "zipcode": {
-#           "type": "integer",
-#           "description": "The 5-digit zip code for the shipping address."
-#         }
-#       },
-#       "required": ["city", "state", "zipcode"]
-#     }
-#   },
-#   "required": ["ordertime", "orderid", "itemid", "orderunits", "address"]
-# }
-
-#     """
+    print("\n\n\t\t\t\t\t\t\t\t\t\t\tConsumer has started!!\n")
+    
+    
     if (schema_location != 'None'):
+    
       with open("schema.json") as f:
+    
           schema_str = f.read()
 
-    if (confluent_sr_config != 'None'):
-         schema_registary =SchemaRegistryClient(read_ccloud_config(args.config_file,'sr'))
-         latest = schema_registary.get_latest_version(f'{topic}-value')
-         schema_str = schema_registary.get_schema(latest.schema_id)
-         schema_str=schema_str.schema_str
-
-
-    #      curl_cmd = [
-    #     "curl",
-    #     "-s",
-    #     "-u",
-    #     f"{api_key}:{api_secret}",
-    #     "GET",
-    #     f"{confluent_url}"
-    # ]
-
-    # # Run the curl command and capture the output
-    # output = subprocess.check_output(curl_cmd)
-
-    # # Print the output
-    # #print(output.decode())
-
-    # schema_dict = json.loads(output)
-
-    # # Pretty-print the dictionary
-    # #print(json.dumps(schema_dict, indent=4))
-
-    # schema_str_1 = schema_dict["schema"]
-    # schema_json = json.loads(schema_str_1)
-
-    # # Print the schema JSON object in a nicely formatted way
-    # schema_str = json.dumps(schema_json, indent=2)
-    # #print(p)
+    
+    if (value_deserializer == 'AvroDeserializer' or 'JSONSchemaDeserializer'):
+    
+        schema_registary =SchemaRegistryClient(read_ccloud_config(args.config_file,'sr'))
+   
+        latest = schema_registary.get_latest_version(f'{topic}-value')
         
+        schema_str = schema_registary.get_schema(latest.schema_id)
+   
+        schema_str=schema_str.schema_str
+         #print(schema_str)
 
-    json_deserializer = JSONDeserializer(schema_str,from_dict=dict_to_user)
+
+    if value_deserializer == 'JSONSchemaDeserializer':         
+
+      #json_deserializer = JSONDeserializer(schema_str,from_dict=dict_to_user)
+      json_deserializer = JSONDeserializer(schema_str=schema_str)
+    
+    
+    elif value_deserializer == 'AvroDeserializer':
+    
+      schema_registry_client = SchemaRegistryClient(read_ccloud_config(args.config_file,'sr'))
+    
+      avro_deserializer = AvroDeserializer(schema_registry_client=schema_registry_client,schema_str=schema_str)
+    
+    
+    elif value_deserializer == 'StringDeserializer':
+    
+       string_deserializer = StringDeserializer(codec='utf_8')
 
     consumer=Consumer(properties)    
+    
     consumer.subscribe([topic])
     
     topic_partitions = [TopicPartition(topic, p) for p in consumer.list_topics(topic).topics[topic].partitions]
+    
     #print(topic_partitions)
     print("Number of partition in the topic:",len(topic_partitions))
 
     total_message = 0
+    
+    
     for tp in topic_partitions:
+    
         total_message=total_message+consumer.get_watermark_offsets(tp)[1]
     
     print("\nTotal Messages in topic\t:\t", total_message)
     
-
     count = 0
+    
     avg = 0
+    
     latency_arry = []
     
     start_time=time.time()
-    elapsed_time=0
     
+    elapsed_time=0    
     
 
     try:
 
         while elapsed_time<run_interval:
+          
             msg = consumer.poll(1.0)
+          
             elapsed_time = time.time()-start_time
+          
             if msg is None:
+          
                 # print(f"Consumer will close in {int(run_interval-elapsed_time)}seconds")
                 continue
+          
+          
             if msg.error():
+          
+          
                 if msg.error().code() == KafkaError._PARTITION_EOF:
+          
                     print(f'Reached end of partition {msg.topic()}-{msg.partition()}')
 
                 else:
+          
                     print(f'Error while consuming from partition {msg.topic()}-{msg.partition()}: {msg.error()}')
+          
             else:
+          
+          
                 #mm = msg.value().decode("utf-8")
                 #print(f'Consumed message from partition {msg.topic()}-{msg.partition()}, offset {msg.offset()}: {msg.value().decode("utf-8")}')
                 #print(mm)
                 #print("i am here")
-                user = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
-
-                if user is not None:
+          
+          
+                if value_deserializer == 'JSONSchemaDeserializer':
+          
+                  user = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+          
+          
+                if value_deserializer == 'AvroDeserializer':
+          
+                  user = avro_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+                # if deserializer == 'StringDeserializer':
+                #     user = string_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+                
+          
+                if ((user is not None) and (value_deserializer == ('JSONSchemaDeserializer' or 'AvroDeserializer'))) :
+                
                     count=count+1
                     
                     # print("Order record {}: \n \tordertime: {}\n"
@@ -299,59 +240,125 @@ if __name__ == '__main__':
                     #     .format(msg.key(), user.ordertime,
                     #             user.itemid,
                     #             user.address))
+                    #print(user)
               
-                    
+                    #print(user['ordertime']) 
                     if t1=="IngestionTime":
                         time1=int(msg.timestamp()[1])
                         
+                
                     elif (t1.split('.')[0]) =='value':
-                        i = t1.split('.')[1]
-                        time1 = getattr(user, i)
+                        #i = t1.split('.')[1]
+                        time1 = user[t1.split('.')[1]]
+                
+                
                     elif (t1.split('.')[0]) == 'key':
-                        i = t1.split('.')[1]
-                        time1 = getattr(user,i)                   
+                
+                        #i = t1.split('.')[1]
+                        #time1 = getattr(user,i)
+                        user1 = json_deserializer(msg.key(), SerializationContext(msg.topic(), MessageField.VALUE))
+                        time1 = user1[t1.split('.')[1]]               
 
                     
-                    
                     if t2 == 'IngestionTime':
+                
                         time2=int(msg.timestamp()[1])
+                
+                
                     elif t2 == 'consumerWallClockTime':
+                
                         time2= time.time()*1000               
                     
                     latency = (time2-time1)
                     latency_arry.append(latency)
+                
+                
+                if ((user is not None) and (value_deserializer == ('StringDeserializer' or 'JSONDeserializer'))):
+                    
+                    count = count+1
+
+                    val = msg.value().decode('utf-8')
+
+                    if t1=="IngestionTime":
+                        time1=int(msg.timestamp()[1])
+                    
+                    else:
+                        mat = re.search(pattern,val)
+                        time1 = int(mat.group(1))
+
+                    
+                    if t2 == 'IngestionTime':
+                
+                        time2=int(msg.timestamp()[1])
+                
+                
+                    elif t2 == 'consumerWallClockTime':
+                
+                        time2= time.time()*1000               
+                    
+                    latency = (time2-time1)
+                    
+                    latency_arry.append(latency)
+
 
 
     except KeyboardInterrupt:
+        
         pass
+    
     finally:
+        
         consumer.close()
         
         
         print("\nTotal Message read by consumer\t:\t",count)
 
+        # with open('numbers.csv', 'w', newline='') as file:
+        #   writer = csv.writer(file)
+
+        #   # Write the array to the CSV file
+        #   writer.writerow(latency_arry)
+
         if sampling == True:
+
             length = int(len(latency_arry)*.3)
+
             random_elements = random.sample(latency_arry,length)
+
             avg=sum(random_elements)//len(random_elements)
+
             print("\nNumber of message sampled(sampling enabled):\t\t",len(random_elements))
+
             print("\nAverage Latency in ms:\t\t\t",avg)
+
+
         elif sampling == False:
+
             avg = sum(latency_arry)//count
+
             print("\nNumber of messages sampled(sampling disabled):\t\t",count)
+
             print("\nAverage latency in ms:\t\t",avg)
+
 
         print("\n\nQuantiles of the latencies measured in ms:")
 
+
         quantiles = np.quantile(latency_arry, [.5, .9, .95, .99, .999])
+
         print("\n\t50th percerntile:\t",quantiles[0])
+
         print("\n\t90th percentile:\t",quantiles[1])
+
         print("\n\t95th percentile:\t",quantiles[2])
+
         print("\n\t99th percentile:\t",quantiles[3])
+
         print("\n\t99.9th percentile:",quantiles[4])
         
 
         if output_type == 'dumpToTopic':
+
           schema_string ="""
             {
               "namespace": "example.avro",
@@ -367,6 +374,7 @@ if __name__ == '__main__':
               ]
             }
             """
+
           result = {
                     "average": int(avg),
                     "percentile50": int(quantiles[0]),
@@ -376,11 +384,14 @@ if __name__ == '__main__':
                     "percentile999": int(quantiles[4])
                   }
 
+
           schema_registry_client = SchemaRegistryClient(conf=read_ccloud_config(args.config_file,'sr'))
+
           avro_serializer = AvroSerializer(schema_registry_client,schema_string)  
+
           producer=Producer(read_ccloud_config(args.config_file,'producer'))
-          producer.produce(topic= output_topic,value=avro_serializer(result, SerializationContext( output_topic, MessageField.VALUE)))
-          
+
+          producer.produce(topic= output_topic,value=avro_serializer(result, SerializationContext( output_topic, MessageField.VALUE)))     
 
 
           # for i, element in enumerate(latency_arry):
@@ -390,13 +401,14 @@ if __name__ == '__main__':
           #   #producer.produce('topic_6', key=i.to_bytes((count.bit_length() + 7)// 8, byteorder='big'), value=element.to_bytes((count.bit_length() + 7)// 8, byteorder='big'))
 
           # # Wait for any outstanding messages to be delivered and delivery reports to be received
+          
           producer.flush()
+          
           print("\n\nData written successfully to :\t",output_topic)
 
         
         if (output_type == 'localFileDump'):
-          
-            
+                      
           df = pd.DataFrame({'average': [avg],
                    'quantile_50': quantiles[0],
                    'quantile_90': quantiles[1],
