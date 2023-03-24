2aeff303a476adfc11e6d51a78ba499bf78ca32f
Arunbbhat <arun.b.bhat@gmail.com>
2023-03-24 17:10:43 +0530

    Deleting unnecessary files

--- producer_latency.py
+++
@@ -1,8 +0,0 @@
-from confluent_kafka import Producer
-
-def latency(latency,config):
-
-    producer=Producer(config)
-    producer.produce("latency-01", value=latency.to_bytes((latency.bit_length() + 7)// 8, byteorder='big'))
-    producer.flush()
-    
